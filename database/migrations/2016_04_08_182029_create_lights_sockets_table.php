afef79bccc8e2aa050c199dab8e8cc0c9fa35c58
Ayrton Maradona <ayrton.maradona@tisstech.com.br>
2016-04-08 15:36:11 -0300

    ajuste no nome da classe da migration gerada

--- database/migrations/2016_04_08_182029_create_lights_sockets_table.php
+++ database/migrations/2016_04_08_182029_create_lights_sockets_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class CreateLights_socketsTable extends Migration
+class CreateLightsSocketsTable extends Migration
 {
     /**
      * Run the migrations.
