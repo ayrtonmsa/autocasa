bc018508e4e6a30b028722390efc5cc0f8921160
ayrton <ayrton.maradona@gmail.com>
2016-04-15 00:14:46 -0300

    ajuste

--- app/House.php
+++ app/House.php
@@ -12,10 +12,8 @@ class House
     {
         $sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
 
-        // Se conecta ao IP e Porta:
         socket_connect($sock,"192.168.25.15", 8081);
 
-        // Verifica a quantidade de itens na casa adiciona a uma string a mensagem que ira enviar para o arduino.
         $lights = Lights_Socket::all();
         $qtdSockets = count($lights);
         $msg='';
@@ -28,16 +26,7 @@ class House
           }
         }
 
-        // if(isset($input['bits'])) {
-        //   $msg = $input['bits'];
-        //   if(isset($input['Fora'   ])){ if($msg[0]=='1') { $msg[0]='0'; } else { $msg[0]='1'; }}
-        //   if(isset($input['Quarto1'])){ if($msg[1]=='1') { $msg[1]='0'; } else { $msg[1]='1'; }}
-        //   if(isset($input['Quarto2'])){ if($msg[2]=='1') { $msg[2]='0'; } else { $msg[2]='1'; }}
-        //   if(isset($input['Sala'   ])){ if($msg[3]=='1') { $msg[3]='0'; } else { $msg[3]='1'; }}
-        //   if(isset($input['Pequeno'])){ $msg = 'P#'; }
-        //   if(isset($input['Grande' ])){ $msg = 'G#'; }
         socket_write($sock,$msg,strlen($msg));
-        // }
 
         return redirect()->back();
     }
