9d82799920b5c56a22f2e2e4f1b710cd3fc720c4
ayrton <ayrton.maradona@gmail.com>
2016-04-19 16:10:37 -0300

    ajuste no codigo arduino para enviar um get e atualizar o status da lampada

--- public/arduino/autocasa/autocasa.ino
+++ public/arduino/autocasa/autocasa.ino
@@ -3,8 +3,8 @@
 
 //Configurações do Ethernet Shield
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-byte ip[] = { 192, 168, 25, 15 }; // ip que o arduino assumirá
-byte gateway[] = { 192, 168, 25, 1 }; // ip do roteador
+byte ip[] = { 192, 168, 9, 31 }; // ip que o arduino assumirá
+byte gateway[] = { 192, 168, 9, 254 }; // ip do roteador
 byte subnet[] = { 255, 255, 255, 0 }; // mascara da red
 EthernetServer server(8081); // Cria o servidor na porta 8081
 
@@ -17,9 +17,6 @@ char Tomada[5] = "00T#";
 // String onde é guardada as msgs recebidas
 char msg[5] = "00M#";
 
-// String que vai enviar os dados para a página
-String data;
-
 void setup() {
   Ethernet.begin(mac, ip, gateway, subnet);
   server.begin();
@@ -30,9 +27,7 @@ void setup() {
 }
 
 void loop() {
-  data = String();
-  data = "";
-
+  
   EthernetClient client = server.available();
   // SE receber um caracter...
   if (client) {
@@ -47,22 +42,35 @@ void loop() {
           Luz[msg[1]] = msg[2];
           switch (msg[1]) {
             case '1':
-              if (Luz[msg[1]] == '1') digitalWrite(A0, HIGH); else digitalWrite(A0, LOW);
-              //data = "&code=" + msg[1];
-              //data += "&status=" + msg[2];
-              //data += "&type=" + msg[3];
-              data = "/code/" + msg[1];
-              data += "/status/" + msg[2];
-              data += "/type/" + msg[3];
+              if (Luz[msg[1]] == '1'){ digitalWrite(A0, HIGH);
+              Serial.println("Connecting");
+  if(clientaux.connect("192.168.9.22",80)){
+      Serial.println("connect");
+      clientaux.print("GET http://192.168.9.22/autocasa/public/house/receiveOfArduino/code/1/status/1/type/L");
+      clientaux.println(" HTTP/1.1");
+      clientaux.println("Host: 192.168.9.22");
+      clientaux.println();
+    }
+  if(clientaux.connected()){
+      clientaux.stop();
+    }
+              } else {digitalWrite(A0, LOW);
+              Serial.println("Connecting");
+  if(clientaux.connect("192.168.9.22",80)){
+      Serial.println("connect");
+      clientaux.print("GET http://192.168.9.22/autocasa/public/house/receiveOfArduino/code/1/status/0/type/L");
+      clientaux.println(" HTTP/1.1");
+      clientaux.println("Host: 192.168.9.22");
+      clientaux.println();
+    }
+  if(clientaux.connected()){
+      clientaux.stop();
+    }
+              }
+              
             break;
             case '2':
               if (Luz[msg[1]] == '1') digitalWrite(A1, HIGH); else digitalWrite(A1, LOW);
-              //data = "&code=" + msg[1];
-              //data += "&status=" + msg[2];
-              //data += "&type=" + msg[3];
-              data = "/code/" + msg[1];
-              data += "/status/" + msg[2];
-              data += "/type/" + msg[3];
             break;
           }
           break;
@@ -72,32 +80,9 @@ void loop() {
           switch (msg[1]) {
             case '1':
               if (Tomada[msg[1]] == '1') digitalWrite(A2, HIGH); else digitalWrite(A3, LOW);
-              //data = "&code=" + msg[1];
-              //data += "&status=" + msg[2];
-              //data += "&type=" + msg[3];
-              data = "/code/" + msg[1];
-              data += "/status/" + msg[2];
-              data += "/type/" + msg[3];
             break;
           }
         break;
-      }
-    }
-    //envia um post para atualizar o banco de dados.
-    if(data != ""){
-      Serial.println("Connecting to Client...");
-      if(clientaux.connect("192.168.25.5:8080", 8080)){
-        Serial.println("Client Connected...");
-     // Make a HTTP request:
-       clientaux.print("GET http://192.168.25.5:8080/autocasa/public/house/receiveOfArduino/code/1/status/1/type/L");
-       clientaux.println(" HTTP/1.1");
-       clientaux.println("Host: 192.168.25.5:8080");
-       clientaux.println();
-      }else{
-        Serial.println("connection failed...");
-      }
-      if(clientaux.connected()){
-        clientaux.stop();
       }
     }
   }
