64dcf4eab7526be4e9c90c08886a0a54c38c06d0
Ayrton Maradona <ayrton.maradona@tisstech.com.br>
2016-04-19 16:04:43 -0300

    ajuste no ip para acesso em outra rede

--- app/House.php
+++ app/House.php
@@ -14,13 +14,12 @@ class House
     {
         $sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
 
-        socket_connect($sock,"192.168.25.15", 8081);
+        socket_connect($sock,"192.168.9.31", 8081);
 
-        //ira enviar sempre 4 caracteres para no maximo 9 dispositivos para cada tipo. 
+         //ira enviar sempre 4 caracteres para no maximo 9 dispositivos para cada tipo.
         //exemplo code 1(lampada 1) status 0(desligar lampada) type L(lamapada) terminador #(caractere de finalização)
         // mensagem enviada 10L#
         $msg = $log['code'].$log['status'].$log['type'].'#';
-
         socket_write($sock,$msg,strlen($msg));
 
         return redirect()->back();
