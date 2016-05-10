cdd910d820e6d3ea4d331ada59c79637083b628a
ayrton <ayrton.maradona@gmail.com>
2016-05-10 18:59:48 -0300

    ajuste

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,26 +22,26 @@
 
 					<div class="panel-body">
 						<div class="row">
-							<a class="small-box-footer" href="{{ url('house/cozinha') }}">
+							<!-- <a class="small-box-footer" href="{{ url('house/cozinha') }}">
 								<div class="col-lg-3 col-xs-6" style="position:absolute; left:65px; top:150px; width:330px; height:280px;" onMouseOver="this.style.backgroundColor='rgba(255,255,0,0.4)'" onMouseOut="this.style.backgroundColor='transparent'">
 								</div>
 							</a>
 							<a class="small-box-footer" href="{{ url('house/banheiro') }}">
 								<div class="col-lg-3 col-xs-6" style="position:absolute; left:400px; top:150px; width:140px; height:280px;" onMouseOver="this.style.backgroundColor='rgba(255,255,0,0.4)'" onMouseOut="this.style.backgroundColor='transparent'">
 								</div>
-							</a>
+							</a> -->
 							<a class="small-box-footer" href="{{ url('house/quarto') }}">
-								<div class="col-lg-3 col-xs-6" style="position:absolute; left:545px; top:150px; width:355px; height:280px;" onMouseOver="this.style.backgroundColor='rgba(255,255,0,0.4)'" onMouseOut="this.style.backgroundColor='transparent'">
+								<div class="col-lg-3 col-xs-6" style="position:absolute; left:120px; top:280px; width:220px; height:180px;" onMouseOver="this.style.backgroundColor='rgba(255,255,0,0.4)'" onMouseOut="this.style.backgroundColor='transparent'">
 								</div>
 							</a>
 							<a class="small-box-footer" href="{{ url('house/sala') }}">
-								<div class="col-lg-3 col-xs-6" style="position:absolute; left:65px; top:435px; width:470px; height:280px;" onMouseOver="this.style.backgroundColor='rgba(255,255,0,0.4)'" onMouseOut="this.style.backgroundColor='transparent'">
+								<div class="col-lg-3 col-xs-6" style="position:absolute; left:600px; top:150px; width:200px; height:430px;" onMouseOver="this.style.backgroundColor='rgba(255,255,0,0.4)'" onMouseOut="this.style.backgroundColor='transparent'">
 								</div>
 							</a>
-							<a class="small-box-footer" href="{{ url('house/quarto2') }}">
+							<!-- <a class="small-box-footer" href="{{ url('house/quarto2') }}">
 								<div class="col-lg-3 col-xs-6" style="position:absolute; left:540px; top:435px; width:365px; height:280px;" onMouseOver="this.style.backgroundColor='rgba(255,255,0,0.4)'" onMouseOut="this.style.backgroundColor='transparent'">
 								</div>
-							</a>
+							</a> -->
 							<!-- <div class="col-lg-3 col-xs-6">
 								<div class="small-box bg-red">
 									<div class="inner"><h3>Área Extra 2</h3></div>
--- resources/views/house/quarto.blade.php
+++ resources/views/house/quarto.blade.php
@@ -4,45 +4,77 @@
     Casa
 @endsection
 
+<style>
+    .featured-image {
+        background: no-repeat center;
+        width: 500px;
+        height: 450px;
+        background-size: cover;
+    }
+</style>
+
 @section('main-content')
     <div class="container spark-screen">
         <div class="row">
             <div class="col-md-10 col-md-offset-1">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Sala</div>
+                    <div class="panel-heading">Quarto</div>
 
                     <div class="panel-body">
                         <div class="row">
-                            @foreach($lights as $light)
-                                <div class="col-lg-3 col-xs-6" align="center">
-                                    <!-- <div class="small-box {{($light->status == 0)?'bg-red':'bg-green'}}">
-                                        <a class="small-box-footer" href="{{ url('lights_sockets/' . $light->id . '/alterarStatus') }}">
-                                            <div class="inner"><h3>{{$light->name}}</h3></div>
-                                        </a>
-                                    </div> -->
-                                    <?php
-                                        if ($light->status == "0") {
-                                            if($light->type == 'L')
-                                            {
-                                                $lamp = "/img/lampada-apagada.jpg";
-                                            }else{
-                                                $lamp = "/img/off.jpg";
-                                            }
+                            <div class="col-lg-3 col-xs-6" align="center" style="position:absolute; left:130px; top:240px;">
+                                <?php
+                                    if ($lights[0]->status == "0") {
+                                        if($lights[0]->type == 'L')
+                                        {
+                                            $lamp = "/img/lampada-apagada.jpg";
                                         }else{
-                                            if($light->type == 'L')
-                                            {
-                                                $lamp ="/img/lampada-acesa.jpg";
-                                            }else{
-                                                $lamp = "/img/on.jpg";
-                                            }
+                                            $lamp = "/img/off.jpg";
                                         }
-                                    ?>
-                                    <a href="{{ url('lights_sockets/' . $light->id . '/alterarStatusOff') }}">
-                                    <img border="0" alt="Lampada" src="{{asset(''.$lamp.'')}}" width="50" height="50">
-                                    <div class="inner"><h6>{{$light->name}}</h6></div>
-                                    </a>
-                                </div>
-                            @endforeach
+                                    }else{
+                                        if($lights[0]->type == 'L')
+                                        {
+                                            $lamp ="/img/lampada-acesa.jpg";
+                                        }else{
+                                            $lamp = "/img/on.jpg";
+                                        }
+                                    }
+                                ?>
+                                <a href="{{ url('lights_sockets/' . $lights[0]->id . '/alterarStatusOff') }}">
+                                <img border="0" alt="Lampada" src="{{asset(''.$lamp.'')}}" width="50" height="50">
+                                <div class="inner"><h6>{{$lights[0]->name}}</h6></div>
+                                </a>
+                            </div>
+                            <div class="col-lg-3 col-xs-6" align="center" style="position:absolute; left:370px; top:390px;">
+                                <?php
+                                    if ($lights[4]->status == "0") {
+                                        if($lights[4]->type == 'L')
+                                        {
+                                            $lamp = "/img/lampada-apagada.jpg";
+                                        }else{
+                                            $lamp = "/img/off.jpg";
+                                        }
+                                    }else{
+                                        if($lights[4]->type == 'L')
+                                        {
+                                            $lamp ="/img/lampada-acesa.jpg";
+                                        }else{
+                                            $lamp = "/img/on.jpg";
+                                        }
+                                    }
+                                ?>
+                                <a href="{{ url('lights_sockets/' . $lights[4]->id . '/alterarStatusOff') }}">
+                                <img border="0" alt="Lampada" src="{{asset(''.$lamp.'')}}" width="50" height="50">
+                                <div class="inner"><h6>{{$lights[4]->name}}</h6></div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="panel-body">
+                        <div class="row">
+                            <div class="featured-image" style="background-image: url('{{ asset('/img/quarto.jpg')}}') ">
+                                <div></div>
+                            </div>
                         </div>
                     </div>
                 </div>
